c209e1dfd644833c8197e6d35e7ef41df3bc581d
yoshikikakehi <yoshikikakehi@gmail.com>
2021-03-29 22:57:54 -0400

    Update quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,20 +1,36 @@
 (function() {
     var questions = [{
+      type: "input",
       question: "How old are you?",
-      choices: ["<20", "21-39", "40-65", ">65"]
     }, {
-      question: "What is 3*6?",
-      choices: [3, 6, 9, 12, 18],
+      type: "radio",
+      question: "What’s your current employment status?",
+      choices: ["   Employed at a company", "   Self-employed", "   Business owner"],
     }, {
-      question: "What is 8*9?",
-      choices: [72, 99, 108, 134, 156],
+      type: "radio",
+      question: "How many employees does your business have?",
+      choices: ["   None/a few",  "   ~10-25", "   ~30-50", "   50+"],
     }, {
-      question: "What is 1*7?",
-      choices: [4, 5, 6, 7, 8],
+      type: "radio",
+      question: "How long have you been in this position?",
+      choices: ["   Recently began (<1 year)", "   1-3 years", "   3-9 years", "   10+ years"]
     }, {
-      question: "What is 8*8?",
-      choices: [20, 30, 40, 50, 64],
-    }];
+      type: "input",
+      question: "What is your modified adjusted gross income? (pre-tax income)",
+    }, {
+      type: "radio",
+      question: "What is your current retirement plan?",
+      choices: ["   401(k)", "   Traditional IRA", "   Roth IRA", "   SIMPLE IRA", "   SEP(SEP-IRA)", "   Other", "   I don’t currently have a retirement plan"],
+    }, {
+      type: "radio",
+      question: "Does your employer offer a company match on your 401(k)?",
+      choices: ["   Yes", "   No"],
+    }, {
+      type: "radio",
+      question: "Do you think your tax rate will be higher or lower when you retire?",
+      choices: ["   Higher", "   Lower", "   I don’t know"],
+    }
+  ];
     
     var questionCounter = 0; //Tracks question number
     var selections = []; //Array containing user choices
@@ -32,14 +48,11 @@
         return false;
       }
       choose();
-      
-      // If no user selection, progress is stopped
-      if (isNaN(selections[questionCounter])) {
-        alert('Please make a selection!');
-      } else {
+      if ((questionCounter === 5 && selections[5] !== 0) || (questionCounter === 1 && selections[1] !== 2)) {
         questionCounter++;
-        displayNext();
       }
+      questionCounter++;
+      displayNext();
     });
     
     // Click handler for the 'prev' button
@@ -88,30 +101,36 @@
       var question = $('<p>').append(questions[index].question);
       qElement.append(question);
       
-      var radioButtons = createRadios(index);
-      qElement.append(radioButtons);
-      
+      if (questions[index].type === 'radio') {
+        var radioButtons = createRadios(index);
+        qElement.append(radioButtons);
+      } else {
+        var inputField = $('<input type="text" id="text" size="20">');
+        qElement.append(inputField);
+      }
       return qElement;
     }
     
     // Creates a list of the answer choices as radio inputs
     function createRadios(index) {
       var radioList = $('<ul>');
-      var item;
       var input = '';
       for (var i = 0; i < questions[index].choices.length; i++) {
-        item = $('<li>');
         input = '<input type="radio" name="answer" value=' + i + ' />';
-        input += questions[index].choices[i];
-        item.append(input);
-        radioList.append(item);
+        input += questions[index].choices[i] + "<br/>";
+        radioList.append(input);
       }
       return radioList;
     }
     
     // Reads the user selection and pushes the value to an array
     function choose() {
-      selections[questionCounter] = +$('input[name="answer"]:checked').val();
+      if (questions[questionCounter].type === 'input') {
+        selections[questionCounter] = +document.getElementById("text").value;
+      } else {
+        selections[questionCounter] = +$("input[name='answer']:checked").val();
+      }
+      console.log(selections[questionCounter]);
     }
     
     // Displays next requested element
@@ -134,7 +153,7 @@
             $('#prev').hide();
             $('#next').show();
           }
-        }else {
+        } else {
           var scoreElem = displayScore();
           quiz.append(scoreElem).fadeIn();
           $('#next').hide();
@@ -146,17 +165,31 @@
     
     // Computes score and returns a paragraph element to be displayed
     function displayScore() {
-      var score = $('<p>',{id: 'question'});
+      var score = $('<p style="font-size:20px">',{id: 'question'});
       
-      var numCorrect = 0;
-      for (var i = 0; i < selections.length; i++) {
-        if (selections[i] === questions[i].correctAnswer) {
-          numCorrect++;
-        }
+      if (selections[1] === 2) {
+        if (selections[2] === 0){
+        score.append('<b>Suggestion</b>:<br/> For your small business, you should set up a SEP IRA for you and your employees.<br/><br/>'
+        + '<b>Benefits</b>:<br/>' 
+        + '<ul><li>A SEP IRA is easy to set up and manage, and doesn’t require too much paperwork with the IRS.</li>'
+        + '<li>It offers you a much higher annual contribution limit than a traditional IRA (almost 10 times as much!) and contributions are also tax-deductible for you AND your employees.</li>'
+        + '<li>Business isn’t doing so well? Have no fear, a SEP IRA doesn’t require you to contribute every year.</li></ul>');
+      } else if (selections[2] === 2){
+        score.append('<b>Suggestion</b>:<br/> For your business, you should set up a SIMPLE IRA for you and your employees. For yourself, consider contributing to your own Roth IRA. <br/><br/>'
+        + '<b>Benefits</b>:<br/>' 
+        + '<ul><li>A SIMPLE IRA is easy to set up and manage, and allows employees to own their own accounts.</li>'
+        + '<li>As compared to your current SEP IRA, a SIMPLE IRA takes the burden of contribution off of you, allowing employees to contribute a portion of their salary to their accounts.</li>'
+        + '<li>Your current contributions to a SIMPLE IRA are tax-deductible!</li>'
+        + '<li>Based on your income and its projected growth, you should save your money in a Roth IRA to take advantage of your relatively lower tax rate now, before you go up a tax bracket. This way, when you retire, only the portion of money in your SIMPLE IRA will be taxed.</li></ul>');
+      } 
+    } else if (selections[1] === 0){
+        score.append('<b>Suggestion</b>:<br/> You should contribute to both your company-sponsored 401(k) and your own Roth IRA.<br/><br/>'
+        + '<b>Benefits</b>:<br/>' 
+        + '<ul><li>Your employer offers a company match on your 401(k), so take advantage of the free money! Contribute enough money to the account to get the maximum match.</li>'
+        + '<li>When you contribute to a Roth IRA now, you will still pay taxes on that money...but when you take it out during retirement in the future, your withdrawals will be tax-free!</li>'
+        + '<li>As a young employee whose salary still has room to grow, take advantage of your lower tax rate now with a Roth IRA so you can avoid paying higher taxes when you withdraw your money later on.</li></ul>');
       }
-      
-      score.append('You got ' + numCorrect + ' questions out of ' +
-                   questions.length + ' right!!!');
+      score.append('</p>');
       return score;
     }
   })();
